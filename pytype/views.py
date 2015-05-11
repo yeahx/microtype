785426d2abeb4a86d409a1aca6668b0648f14fbf
calvinxu <wings_of_fr3edom@hotmail.com>
2015-05-11 21:42:57 +0800

    remove views.py file

--- pytype/views.py
+++
@@ -1,7 +0,0 @@
-from pytype import app
-from flask import render_template
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title = "Nightraid\'s blog")
