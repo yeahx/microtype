ce717a892964adee341439868f5e8c8785577707
lazydog <wings_of_fr3edom@hotmail.com>
2015-11-23 22:49:00 +0800

    delete pytype directory

--- pytype/static/js/navbar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    url = window.location.href.split('/')[3];
-    if(url == "index" or == ""){
-        $("#index").attr("class","am-active");
-    }
-    else if(url == "links"){
-        $("#links").attr("class","am-active");
-    }
-});
